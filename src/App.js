eacc97f0b9ce7e1bde9e82c1260b19c30d2aa0ad
Scott Wilson <swrs@hotmail.co.uk>
2022-06-09 12:22:47 +0100

    set up .env and fetch in app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
 import "./App.css";
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState("apple");
+
+  useEffect(() => {
+    async function FetchStories() {
+      const response = await fetch(
+        `https://gnews.io/api/v4/search?q=${searchQuery}&token=${process.env}`
+      );
+      const data = await response.json();
+    }
+    FetchStories();
+  }, [searchQuery]);
+
   return <div className="App"></div>;
 }
 
